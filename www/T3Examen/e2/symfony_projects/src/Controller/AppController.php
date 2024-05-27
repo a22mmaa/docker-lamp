c135ea22932b01fb858ba723d3a820febc354cbb
Nombre de alumno <nombre@alumno>
2024-05-27 21:32:30 +0200

    Exercicio 2 feito

--- www/T3Examen/e2/symfony_projects/src/Controller/AppController.php
+++ www/T3Examen/e2/symfony_projects/src/Controller/AppController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use function Symfony\Component\String\u;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Psr\Cache\CacheItemInterface;
 
 class AppController extends AbstractController
 {
@@ -13,18 +15,49 @@ class AppController extends AbstractController
     #[Route('/')]
     public function homepage()
     {
-        return $this->render('base.html.twig', [
+        return $this->render('pets.html.twig', [
             'title' => 'Inicio',
         ]);
     }
     */
 
     #[Route('/pets')]
-    public function mostrarDatos()
+    public function listar(HttpClientInterface $httpClient, string $slug = null): Response
     {
-        return $this->render('base.html.twig', [
-            'title' => 'Inicio',
+        $title = 'Pets';
+
+        $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/a22mmaa/docker-lamp/main/www/T3Examen/pets.json');
+
+        $pets = $response->toArray();
+
+        return $this->render('pets.html.twig', [
+            'title' => $title,
+            'pets' => $pets
         ]);
+
+        return new Response('Titulo: ' . $title);
+    }
+
+    #[Route('/petscache')]
+    public function fasvs(
+        HttpClientInterface $httpClient,
+        CacheInterface $cache,
+        string $xenero = null
+        ): Response
+    {
+        $title = 'Pets cache';
+        $response = $cache->get('pets_data', function(CacheItemInterface $cacheItem) use ($httpClient) {
+            $cacheItem->expiresAfter(2);
+            $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/a22mmaa/docker-lamp/main/www/T3Examen/pets.json');
+            return $response->toArray();
+        });
+
+        return $this->render('pets.html.twig', [
+            'title' => $title,
+            'pets' => $response
+        ]);
+
+        return new Response('Xénero: ' . $title);
     }
 
 
